1ef500f28e2ac22af7a14ec3d5d64e676bfcb28d
Benny <Benny_Stark@live.de>
2017-01-30 19:25:23 +0100

    Added WifiManager / Added CUL Reset
    CUL Reset at D0/GPIO16
    Bugfixes
    Going slowly from POC to WIP

--- main/main.ino
+++ main/main.ino
@@ -7,25 +7,32 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 
-
+//needed for WifiManager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>  
 
 //Wifi
 const int ServerPort  = 23;
-const char* WifiSSID      = "NewBigBoss";
-const char* WifiPassword  = "Waldgeist99";
-#define WIFIWAITTIME       20 //Sekunden
 
 //CUL Interface
 #define I2CFREQ         400000
 #define CULCOUNT        3
 const char CULADDR[CULCOUNT] = {0x7e, 0x7d, 0x7c};
-#define MAXMSGSIZE      128
+#define MAXMSGSIZE      128 // Maxsize of a CUL Message
+#define I2CBYTECOUNT    8  // Get this amount of Bytes at each read request
+
+#define CUL_BOOTLOADER_WAIT 10 //Sec. to wait after Releasing reset for CUL Bootloader to process
 
 #define LED_PIN         2  //WEMOS D4 Buildin LED/Pull-Up
 #define LED_OFF         digitalWrite(LED_PIN,HIGH)
 #define LED_ON          digitalWrite(LED_PIN,LOW)
 
 #define BUTTON_PIN      0  //WEMOS D3 Pull-Up
+
+#define RESET_PIN       16  //WEMOS D0
+#define RESET_HOLD      digitalWrite(RESET_PIN,HIGH)
+#define RESET_RELEASE   digitalWrite(RESET_PIN,LOW)
 
 
 //WifiServer
@@ -37,13 +44,14 @@ int SocketToI2CCount = 0 ;
 char I2CToSocket[CULCOUNT][MAXMSGSIZE];
 int I2CToSocketCount[CULCOUNT];
 
-
+bool hasTCPClient = false;
 
 void setup() {
   // Debug Console auf HW Serial 
   Serial.begin(115200);
 
-  //Infos
+  //General Info
+
   Serial.println("ESP-Multi-I2CCUL-Bridge by B.Stark");
   Serial.println("Version: Proof of Concept");
   Serial.print("ESP ChipID: ");
@@ -51,23 +59,21 @@ void setup() {
   Serial.print("ESP FreeHeap: ");
   Serial.println(ESP.getFreeHeap());
 
-  //Wifi Verbinden
-  WiFi.begin(WifiSSID,WifiPassword);
-  Serial.print("Verbinde zu SSID: ");
-  Serial.print(WifiSSID);
-  //warten auf Wifi
-  int ConCount = 0;
-  while ( WiFi.status() != WL_CONNECTED && ConCount++ < WIFIWAITTIME) {
-    Serial.print(".");
-    delay(1000);  
-  }
-  if ( ConCount == WIFIWAITTIME + 1 ) {
-    Serial.println("Keine Verbindung");
-    Serial.println(WiFi.status());
-    // RESTART
-    while(true);
-  }
-  Serial.println("OK");
+  //Hold Reset of CULS low
+  pinMode(RESET_PIN,OUTPUT);
+  RESET_HOLD;
+  Serial.println("Hold CUL in Reset");
+
+  // Button for WifiManager Reset
+  pinMode(BUTTON_PIN,INPUT);
+  
+  Serial.println("Starting WifiManager");
+  //WifiManager verbindungen
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("ESP-I2CCUL-Bridge");
+  Serial.println("Connected!");
+
+    //Infos
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
   Serial.print("Subnet Mask: ");
@@ -103,9 +109,7 @@ void setup() {
 
   //Init LED
   ledInit();
-
-  pinMode(BUTTON_PIN,INPUT);
-  
+ 
 }
 
 void ledInit() {
@@ -120,14 +124,27 @@ void handleClientConnection() {
     if (!TCPClient.connected()){
       TCPClient = TCPServer.available();
       Serial.println("Client Verbunden");
+      hasTCPClient = true;
       LED_ON;
+      RESET_RELEASE;
+      Serial.println("Release CUL Reset"); 
+      Serial.print("Wait ");
+      Serial.print(CUL_BOOTLOADER_WAIT);
+      Serial.println(" Sec. for CUL Bootloader to process");
+      delay(CUL_BOOTLOADER_WAIT * 1000);
+      Serial.println("Start processing");
     } else { // ansonsten ablehnen
       WiFiClient TCPClientStop = TCPServer.available();
-      LED_OFF;
       Serial.println("Verbindung abgewiesen");
     }
   }
-  if (!TCPClient.connected()) LED_OFF;
+  if (!TCPClient.connected() && hasTCPClient) { //hasTCPClient as helper for previously connected Clients
+    hasTCPClient = false;
+    LED_OFF;
+    RESET_HOLD;
+    Serial.println("Client disconnected");
+    Serial.println("Hold CUL in Reset");
+  }
 }
 
 void handleSocketData(char data) {
@@ -147,7 +164,7 @@ void handleSocketData(char data) {
     //Direkt SocketToI2CCount zu modifizieren sollte nicht so speicher intensiv sein
     char Msg[MAXMSGSIZE+2]; // \n und \0
     int j=0;
-    for (int pos=i;pos < SocketToI2CCount+1; pos++)
+    for (int pos=i;pos < SocketToI2CCount; pos++) //+1
       Msg[j++]=SocketToI2C[pos];
     Msg[j++]='\n';
     Msg[j]= '\0'; //Ende
@@ -157,7 +174,7 @@ void handleSocketData(char data) {
     Serial.print(" Error: ");
     Serial.print(error);
     Serial.print(" Msg: ");
-    Serial.println(Msg);
+    Serial.print(Msg);
     SocketToI2CCount = 0;
     //Wenn Bufferoverflow
    } else if (SocketToI2CCount == MAXMSGSIZE-1) {
@@ -170,6 +187,7 @@ void handleSocketData(char data) {
 }
 
 bool handleI2CData(int CulNo, char data) {
+    bool returnCULBufferIsEmpty = false;
       if ( data == '\n' ) {
         //senden
         Serial.print("I2CToSocket: ");
@@ -196,6 +214,7 @@ bool handleI2CData(int CulNo, char data) {
       } else if ( data == 0x04) {
         // keine weiteren nachrichten
         //nicht schreiben nicht erhöhen
+        returnCULBufferIsEmpty = true;
       } else if (I2CToSocketCount[CulNo] == MAXMSGSIZE-1) {
         Serial.println("I2CToSocket Buffer Überlauf");
         I2CToSocketCount[CulNo] = 0;
@@ -203,6 +222,7 @@ bool handleI2CData(int CulNo, char data) {
           // in den Buffer schreiben
           I2CToSocket[CulNo][I2CToSocketCount[CulNo]++] = data;
       }
+      return returnCULBufferIsEmpty;
 }
 
 void loop() {
@@ -211,16 +231,26 @@ void loop() {
   if (TCPClient.connected()) {
     while (TCPClient.available())
       handleSocketData(TCPClient.read());
-
     //i2c lesen
     for (int i=0; i < CULCOUNT; i++) {
       //Read 32bit at once - Arduino workaround
-      Wire.requestFrom(CULADDR[i],32);
-      // Daten holen bis der CUL Buffer leer ist
-      while(Wire.available()) 
-        handleI2CData(i,Wire.read());
+      bool culBufferWasEmpty = false;
+      while(!culBufferWasEmpty) {  
+        Wire.requestFrom(CULADDR[i],I2CBYTECOUNT);
+        // Daten holen bis der CUL Buffer leer ist
+        if(!Wire.available()) culBufferWasEmpty = true; //Not Empty but nothing received
+        while(Wire.available()) {
+          bool ret = handleI2CData(i,Wire.read());
+          if(ret) culBufferWasEmpty = true;
+        }
+      } // while(culBufferWasEmpty)
     }//for
    }//if (TCPClient.connected())
 
-  if (digitalRead(BUTTON_PIN) == LOW) Serial.println("Button gedrückt");
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    Serial.println("Reset Setting and restart");
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    ESP.restart();   
+  }
 }
